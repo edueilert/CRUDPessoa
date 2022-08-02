c435d7644913bf554fb1dd0df278da59380d027e
Eduardo Rodrigues <eduardofutrodrigues@hotmail.com>
2022-08-02 17:23:45 -0300

    aula 6.1

--- view/listaUsuario.php
+++ view/listaUsuario.php
@@ -11,12 +11,14 @@ $listaUsers = $cadUsuario->getUsuario();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="../assets/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
     <title>Document</title>
 </head>
 <body>
+    <div class="conatainer">
 <div id="lista">
         <h2>Lista Usuários</h2>
-        <table>
+        <table class="table-striped">
             <thead>
                 <tr>
                     <th>
@@ -62,5 +64,7 @@ $listaUsers = $cadUsuario->getUsuario();
         </table>
         <input type="button" value="Voltar" onclick="location.href='CadUsuario.php'">
     </div>
+    </div>
+    <script src="assets/js/bootstrap.bundle" crossorigin="anonymous"> </script>
 </body>
 </html>
